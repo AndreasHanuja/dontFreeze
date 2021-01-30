4cd3d31a429b8515b999970289767cdfd4062336
Andreas Hanuja <hanuja2@gmail.com>
2021-01-31 00:01:47 +0100

    wolf

--- Assets/Scripts/Wolf.cs
+++ Assets/Scripts/Wolf.cs
@@ -4,13 +4,16 @@ using UnityEngine;
 
 public class Wolf : MonoBehaviour
 {
-    enum WolfStates { IDLE, CHAISE, CIRCLE, ATTACK, FEARFIRE}
+    public enum WolfStates { IDLE, CHAISE, CIRCLE, ATTACK, FEARFIRE}
 
 
-    public Transform player;
-    private WolfStates currentMode;
+    public WolfStates currentMode;
+    private float moveStartTime;
     private float circleStartTime;
     private bool circleClockwise;
+    private Vector3 spawnPoint;
+
+    public Transform player;
 
     public List<GameObject> campFires;
 
@@ -19,18 +22,39 @@ public class Wolf : MonoBehaviour
     public float circleDistance;
     public float circleDuration;
     public float fearFireDistance;
+    public float maxMoveTime;
+    public float maxIdleDistance;
 
     private Vector3 currentTargetPoint;
 
-    private float currentMoveSpeed;
-    private float currentRotationSpeed;
+    public float currentMoveSpeed;
+    public float currentRotationSpeed;
+
+    private void Start() {
+        spawnPoint = transform.position;
+    }
 
     private void Update()
     {
+        WolfStates oldState = currentMode;
         UpdateModes();
-        //MoveForward();
+        if (currentMode != oldState)
+        {
+            GiveMoveCommand();
+        }
+
+        if(Time.realtimeSinceStartup - moveStartTime > maxMoveTime)
+        {
+            GiveMoveCommand();
+        }
+
+        if(Vector3.Magnitude(currentTargetPoint - transform.position) < 1)
+        {
+            GiveMoveCommand();
+        }
 
         RotateTowardsPoint();
+        MoveForward();
     }
 
     private void UpdateModes()
@@ -44,7 +68,6 @@ public class Wolf : MonoBehaviour
             if(currentMode == WolfStates.IDLE)
             {
                 currentMode = WolfStates.CHAISE;
-                circleClockwise = Random.Range(0, 2) < 1;
             }
         }
 
@@ -52,6 +75,7 @@ public class Wolf : MonoBehaviour
         {
             currentMode = WolfStates.CIRCLE;
             circleStartTime = Time.timeSinceLevelLoad;
+            circleClockwise = Random.Range(0, 2) < 1;
         }
 
         if (currentMode == WolfStates.CHAISE && Vector3.SqrMagnitude(player.transform.position - transform.position) < circleDistance * circleDistance)
@@ -75,12 +99,51 @@ public class Wolf : MonoBehaviour
         }
     }
 
+    private void GiveMoveCommand()
+    {
+        moveStartTime = Time.realtimeSinceStartup;
+
+        switch (currentMode)
+        {
+            case WolfStates.IDLE:
+                currentTargetPoint = spawnPoint + new Vector3(Random.Range(-1, 1), 0, Random.Range(-1, 1)).normalized * Random.Range(0, maxIdleDistance);
+                break;
+            case WolfStates.CHAISE:
+                currentTargetPoint = player.transform.position;
+                break;
+            case WolfStates.CIRCLE:
+                //currentTargetPoint = player.transform.position;
+                Vector3 difPoint = player.transform.position-transform.position;
+                difPoint.y = 0;
+                difPoint.Normalize();
+                //transform.for
+                float goalRotation = Mathf.Atan2(difPoint.z, difPoint.x) * Mathf.Rad2Deg + 15;
+                Vector3 goalPosition = new Vector3(Mathf.Cos(goalRotation * Mathf.Deg2Rad), 0, Mathf.Sin(goalRotation * Mathf.Deg2Rad));
+                currentTargetPoint = player.transform.position - goalPosition * circleDistance;
+                break;
+            case WolfStates.ATTACK:
+                //currentTargetPoint = player.transform.position;
+                currentTargetPoint = player.transform.position;
+                break;
+        }
+    }
+
     private void RotateTowardsPoint()
     {
-        Vector3 difPoint = player.transform.position - transform.position;
+        Vector3 difPoint = currentTargetPoint - transform.position;
         difPoint.y = 0;
         difPoint.Normalize();
-        Debug.Log(Mathf.Asin(difPoint.x));
+        //transform.for
+        float goalRotation = Mathf.Atan2(difPoint.x, difPoint.z) * Mathf.Rad2Deg + 360;
+        goalRotation = (360+(goalRotation - transform.rotation.eulerAngles.y))%360;
+
+        if(goalRotation > 180)
+        {
+            goalRotation = goalRotation - 360;
+        }
+
+        float targetY = Mathf.Clamp(goalRotation, -currentRotationSpeed * Time.deltaTime, currentRotationSpeed * Time.deltaTime);
+        transform.Rotate(new Vector3(0,targetY,0),Space.World);
 
     }
     private void MoveForward()
