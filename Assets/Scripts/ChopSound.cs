fbdbe783396dc4b15eb444333904f7bab0e575e4
Rakus123 <49484676+Rakus123@users.noreply.github.com>
2021-01-31 15:31:46 +0100

    soundscript

---
+++ Assets/Scripts/ChopSound.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChopSound : MonoBehaviour
+{
+    public static bool playWolf;
+    public static bool playChop;
+
+    public AudioSource wolfAS;
+    public AudioSource chopAS;
+
+    // Update is called once per frame
+    void Update()
+    {
+        playChopAS();
+        playWolfAS();
+    }
+
+    void playChopAS()
+    {
+        if(playChop)
+        {
+            chopAS.Play();
+            playChop = false;
+        }
+    }
+
+    void playWolfAS()
+    {
+        if(playWolf)
+        {
+            wolfAS.Play();
+            playWolf = false;
+        }
+    }
+}
