59a65c46e8aa88a0886e59bb2a4fd87b159664df
Narek Minaskan <narek.nmk@gmail.com>
2021-01-31 13:28:58 +0100

    Random waypoint direction

--- Assets/Scripts/Waypoint.cs
+++ Assets/Scripts/Waypoint.cs
@@ -5,14 +5,24 @@ public class Waypoint : MonoBehaviour
 {
     private bool isRotatedCorrectly = false;
 
+    Vector3 offsetPoisiton; 
+
+    private void Awake()
+    {
+        offsetPoisiton = new Vector3(Random.Range(-20, 20), 0, Random.Range(-20, 20));
+    }
+
     void Update()
     {
-        if(MapGenerator.house != null && isRotatedCorrectly)
+        
+        if (MapGenerator.house != null && isRotatedCorrectly)
         {
-            //transform.position -> this waypoint
-            //MapGenerator.house.transform.position
-
-            //magicaly set the rotation (maybe clamped to 45 degrees)
+            Vector3 newHouseOffsetPosition = MapGenerator.house.transform.position + offsetPoisiton;
+            Quaternion rotationAngle = Quaternion.LookRotation(newHouseOffsetPosition - transform.position);
+            transform.rotation = rotationAngle;
+                  
         }
+
+        isRotatedCorrectly = true;
     }
 }
